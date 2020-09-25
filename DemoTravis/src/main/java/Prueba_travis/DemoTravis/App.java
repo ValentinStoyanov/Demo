41c2c37d740e161395b32aed828ad8b55e312335
Valentin <Valentin@DESKTOP-56KBONL>
2020-09-25 16:36:05 +0200

    cgbb

---
+++ DemoTravis/src/main/java/Prueba_travis/DemoTravis/App.java
@@ -0,0 +1,13 @@
+package Prueba_travis.DemoTravis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
